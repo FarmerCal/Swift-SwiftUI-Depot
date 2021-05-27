89f7035b57509489476560cf5b4378135b92e5b4
callum day ham <caldayham@gmail.com>
2021-05-26 20:46:08 -0700

    Initial Commit

---
+++ Swift-SwiftUI-Depot/Swift_SwiftUI_DepotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SwiftUI_DepotApp.swift
+//  Swift-SwiftUI-Depot
+//
+//  Created by Callum Day Ham on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_SwiftUI_DepotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
