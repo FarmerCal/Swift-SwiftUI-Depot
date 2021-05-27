8b995c2c77f5adea609c54c416389d79374fdccc
callum day ham <caldayham@gmail.com>
2021-05-26 20:50:08 -0700

    - create main json files - compose general folder layout and hierarchy

---
+++ Swift-SwiftUI-Depot/Services/JSON_Parsing.swift
@@ -0,0 +1,8 @@
+//
+//  JSON_Parsing.swift
+//  Swift-SwiftUI-Depot
+//
+//  Created by Callum Day Ham on 5/26/21.
+//
+
+import Foundation
---
+++ Swift-SwiftUI-Depot/ViewModels/NoteModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoteModel.swift
+//  Swift-SwiftUI-Depot
+//
+//  Created by Callum Day Ham on 5/26/21.
+//
+
+import Foundation
